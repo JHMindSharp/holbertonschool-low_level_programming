0bb88251fd1d1ac7647cebd323ce3664b3b717df
Miniknacky <Jonathanhuybrechts@gmail.com>
2023-10-12 13:57:40 -0700

    prints the size

---
+++ hello_world/6-size.c
@@ -0,0 +1,26 @@
+/*
+ * File 6-size.c
+ * A C program that prints the size of various types on the computer
+ *
+ * Warning are allowed.
+ * The program returns 0.
+ */
+
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: This program prints the size of various data types.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	printf("Size of a char: %lu byte(s)\n", sizeof(char));
+	printf("Size of an int: %lu byte(s)\n", sizeof(int));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
+
+	return (0);
+}
