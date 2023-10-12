b6e039ae287601dc351b695b0d8105ef5d5ade34
Miniknacky <Jonathanhuybrechts@gmail.com>
2023-10-12 13:34:24 -0700

    ok

---
+++ hello_world/5-printf.c
@@ -0,0 +1,17 @@
+/*
+ * File 5-printf.c
+ * Auth: Huybrechts J
+ */
+#include <stdio.h>
+
+/**
+ * main - Prints with proper grammar..  followed by a lew line.
+ *
+ * Return: always 0.
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
+
