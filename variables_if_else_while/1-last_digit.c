7d7e40c1a05c81570d49329bac59c0f189df3565
Miniknacky <Jonathanhuybrechts@gmail.com>
2023-10-16 11:53:11 -0700

    space

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -5,40 +5,38 @@
 /**
  * main - Entry point
  *
- * Description: This program generates a random number, calculates its last digit,
+ * Description:Generates a random number, calculates its last digit,
  * and prints different messages based on the value of the last digit.
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-    int n;
-    int lastDigit; /* Déclaration de la variable au début de la fonction */
+	int n;
+	int lastDigit; /* Déclaration de la variable au début de la fonction */
 
-    srand(time(0));
-    n = rand() - RAND_MAX / 2;
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	printf("The string Last digit of, followed by\n");
+	printf("%d is %d and is ", n, lastDigit); /* Modification de cette ligne */
 
-    printf("The string Last digit of, followed by\n");
-    printf("%d, followed by\n", n);
-    printf("the string is, followed by\n");
+	lastDigit = n % 10; /* Assignation de la valeur à la variable */
 
-    lastDigit = n % 10; /* Assignation de la valeur à la variable */
+	if (lastDigit > 5)
+	{
+		printf("greater than 5\n"); /* Modification de cette ligne */
+	}
+	else if (lastDigit == 0)
+	{
+		printf("0\n"); /* Modification de cette ligne */
+	}
+	else
+	{
+		printf("less than 6 and not 0\n"); /* Modification de cette ligne */
+	}
 
-    if (lastDigit > 5)
-    {
-        printf("if the last digit of n is greater than 5: the string and is greater than 5\n");
-    }
-    else if (lastDigit == 0)
-    {
-        printf("if the last digit of n is 0: the string and is 0\n");
-    }
-    else
-    {
-        printf("if the last digit of n is less than 6 and not 0: the string and is less than 6 and not 0\n");
-    }
+	printf("followed by a new line\n");
 
-    printf("followed by a new line\n");
-
-    return (0);
+	return (0);
 }
 
