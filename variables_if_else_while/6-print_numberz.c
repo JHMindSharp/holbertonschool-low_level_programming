b9e3f5a08c51c53e4e63d1a480b6f4c67635a736
Miniknacky <Jonathanhuybrechts@gmail.com>
2023-10-17 06:06:12 -0700

    putchar and not printf

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,35 @@
+
+#include <stdio.h>
+
+/**
+ * main -  prints all single digit numbers of base 10 starting from 0.
+ *
+ * description:this is printing all digit of 10 starting from 0 .
+ *
+ * Return: (0) ins the value.
+ */
+int main(void)
+{
+	putchar('0');
+	putchar('\n');
+	putchar('1');
+	putchar('\n');
+	putchar('2');
+	putchar('\n');
+	putchar('3');
+	putchar('\n');
+	putchar('4');
+	putchar('\n');
+	putchar('5');
+	putchar('\n');
+	putchar('6');
+	putchar('\n');
+	putchar('7');
+	putchar('\n');
+	putchar('8');
+	putchar('\n');
+	putchar('9');
+	putchar('\n');
+
+	return (0);
+}
