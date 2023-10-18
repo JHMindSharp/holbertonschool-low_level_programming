efa0cb7e9902dd5d58f5bcccd42d469df0f9c6aa
Miniknacky <Jonathanhuybrechts@gmail.com>
2023-10-18 04:50:14 -0700

    school

---
+++ makefiles/makefiles/school.c
@@ -0,0 +1,13 @@
+
+#include <stdio.h>
+
+/**
+ * print_school - will print the line.
+ *
+ * description: This function will print the line.
+ */
+void print_school(void)
+{
+	printf("I am a student at Holberton School!\n");
+}
+
