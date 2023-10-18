42cc223f8a59f9ce94681ae9fc6a4bdac2ebd713
Miniknacky <Jonathanhuybrechts@gmail.com>
2023-10-18 03:37:17 -0700

    24h

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,36 @@
+
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * jack_bauer - Prints every minute of the day from 00:00 to 23:59.
+ */
+void jack_bauer(void)
+{
+	int h1, h2, m1, m2;
+
+	for (h1 = 0; h1 <= 2; h1++)
+	{
+		for (h2 = 0; h2 <= 9; h2++)
+		{
+			for (m1 = 0; m1 <= 5; m1++)
+			{
+				for (m2 = 0; m2 <= 9; m2++)
+				{
+					if (h1 == 2 && h2 > 3)
+					{
+						break;
+					}
+
+					_putchar(h1 + '0');
+					_putchar(h2 + '0');
+					_putchar(':');
+					_putchar(m1 + '0');
+					_putchar(m2 + '0');
+					_putchar('\n');
+				}
+			}
+		}
+	}
+}
+
---
+++ functions_nested_loops/8-main.c
@@ -0,0 +1,15 @@
+
+#include "main.h"
+/**
+ * main - calling jack_ bauer function.
+ *
+ * description: file who are running jack bouer function.
+ *
+ * Return: (0) is the value.
+ */
+int main(void)
+{
+	jack_bauer();
+	return (0);
+}
+
