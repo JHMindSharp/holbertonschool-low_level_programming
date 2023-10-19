a6783d60352037674111cf2ddbb10aee90e8839f
Miniknacky <Jonathanhuybrechts@gmail.com>
2023-10-19 11:08:56 -0700

    print square

---
+++ more_functions_nested_loops/8-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/* Prototype declaration */
+void print_diagonal(int size);
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	print_square(2);
+	print_square(10);
+	print_square(0);
+	return (0);
+}
