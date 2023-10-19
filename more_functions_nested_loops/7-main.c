f878b5aab36b2402b7c43783fa031ccec7331967
Miniknacky <Jonathanhuybrechts@gmail.com>
2023-10-19 10:59:32 -0700

    print diagonal

---
+++ more_functions_nested_loops/7-main.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/* Prototype declaration */
+void print_diagonal(int n);
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	print_diagonal(0);
+	print_diagonal(2);
+	print_diagonal(10);
+	print_diagonal(-4);
+	return (0);
+}
+
