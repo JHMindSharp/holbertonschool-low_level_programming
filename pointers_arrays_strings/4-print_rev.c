6b02c51afd69479f940dd3fefc5876e73dccc9cc
Miniknacky <Jonathanhuybrechts@gmail.com>
2023-10-24 01:49:40 -0700

    string revers

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * print_rev - Function that print a string in reverse.
+ * @s: Pointer to the string.
+ */
+void print_rev(char *s)
+{
+	int longueur = strlen(str);
+
+	for (int i = longueur - 1; i >= i--)
+	{
+		putchar(str[i]);
+	}
+
+	putchar('\n');
+}
