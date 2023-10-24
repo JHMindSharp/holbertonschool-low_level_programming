96b8051b9d3e5d9f3728d55150bff70ab1835010
Miniknacky <Jonathanhuybrechts@gmail.com>
2023-10-24 04:01:19 -0700

    ok

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -24,18 +24,4 @@ void print_rev(char *s)
 	}
 	putchar('\n');
 }
-/**
- * main - The function print str pointer.
- *
- * Return: 0 is the value.
- */
-int main(void)
-{
-	char *str;
-
-	str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
-	print_rev(str);
-
-	return (0);
-}
 
