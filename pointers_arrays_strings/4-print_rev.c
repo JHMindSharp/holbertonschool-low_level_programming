bea04fcd3493f684b45a7ad6fc8cec3ba0b88a94
Miniknacky <Jonathanhuybrechts@gmail.com>
2023-10-24 03:52:00 -0700

    ok

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -6,7 +6,19 @@
  */
 void print_rev(char *s)
 {
-	for (int i = str - 1; i >= 0; i--)
+	if (s == NULL)
+	{
+		return;
+	}
+
+	int length = 0;
+
+	while (s[length] != '\0')
+	{
+		length++;
+	}
+
+	for (int i = lenght - 1; i >= 0; i--)
 	{
 		putchar(s[i]);
 	}
