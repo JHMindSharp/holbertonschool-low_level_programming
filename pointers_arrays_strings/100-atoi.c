8daa7946c9df9b8d8d0d6f3c2505bac8c8bda017
Miniknacky <Jonathanhuybrechts@gmail.com>
2023-10-25 04:07:58 -0700

    atoi

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * _atoi - Converta string to an integer.
+ * @s: The string to convert in integers.
+ *
+ * Return: The value int convert.
+ */
+int _atoi(char *s)
+{
+	int sign = 1;
+	int result = 0;
+	int i = 0;
+	int is_digit = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] == '-')
+			sign *= -1;
+		else if (s[i] >= '0' && s[i] <= '9')
+		{
+			is_digit = 1;
+			result = (result * 10) + (s[i] - '0');
+		}
+		else if (is_digit)
+			break;
+		i++;
+	}
+
+	return (result * sign);
+}
+
