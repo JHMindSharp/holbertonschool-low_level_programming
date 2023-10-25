09297c52aadfd9750986891d1b7a5e06283b2fec
Miniknacky <Jonathanhuybrechts@gmail.com>
2023-10-25 02:09:11 -0700

    main

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -2,10 +2,10 @@
 #define MYHEADER_H
 
 void reset_to_98(int *n);
-void swap_int(int *a, int *b)
-void _strlen(str)
-void _puts(char *str)
-void print_rev(char *s)
-void rev_string(char *s)
+void swap_int(int *a, int *b);
+void _strlen(str);
+void _puts(char *str);
+void print_rev(char *s);
+void rev_string(char *s);
 
 #endif
