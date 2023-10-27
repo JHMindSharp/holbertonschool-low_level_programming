c42078744fb48e14f557e65e468aafdfc3c5bcad
Miniknacky <Jonathanhuybrechts@gmail.com>
2023-10-27 05:45:34 -0700

    ok

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -7,8 +7,9 @@
 int is_separator(char c)
 {
 	char separators[] = " \t\n,;.!?\"(){}";
+	int i;
 
-	for (int i = 0; separators[i]; i++)
+	for (i = 0; separators[i] != '\0'; i++)
 	{
 		if (c == separators[i])
 		{
@@ -16,38 +17,6 @@ int is_separator(char c)
 		}
 	}
 
-	return (0);
-}
-
-/**
- * cap_string - Capitalize all words in a string
- * @str: Input string
- *
- * Return: Pointer to the modified string
- */
-char *cap_string(char *str)
-{
-	char *ptr = str;
-	int capitalize_next = 1;
-
-	while (*str != '\0')
-	{
-		if (capitalize_next && *str >= 'a' && *str <= 'z')
-		{
-			*str = *str - 32;
-			capitalize_next = 0;
-		}
-		else if (is_separator(*str))
-		{
-			capitalize_next = 1;
-		}
-		else
-		{
-			capitalize_next = 0;
-		}
-		str++;
-	}
-
-	return (ptr);
+	return 0;
 }
 
