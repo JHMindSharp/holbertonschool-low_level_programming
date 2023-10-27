cb7c9081412c00b800779294cccd2f35526a7cb6
Miniknacky <Jonathanhuybrechts@gmail.com>
2023-10-27 07:33:48 -0700

    space

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -5,11 +5,12 @@
  * @s: Le pointeur vers la mémoire à remplir.
  * @b: L'octet constant pour remplir la mémoire.
  * @n: Le nombre d'octets à remplir.
- * 
+ *
  * Return: Un pointeur vers la mémoire remplie.
  */
 char *_memset(char *s, char b, unsigned int n)
 {
+
 	unsigned int i;
 
 	for (i = 0; i < n; i++)
