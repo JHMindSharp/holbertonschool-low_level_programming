231a9a64349fdd0765233578e5d07218437d5709
Miniknacky <Jonathanhuybrechts@gmail.com>
2023-10-27 05:52:57 -0700

    error

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -1,22 +1,41 @@
 /**
- * is_separator - Check if a character is a word separator
- * @c: The character to check
+ * cap_string - Capitalize all words in a string
+ * @str: Input string
  *
- * Return: 1 if c is a separator, 0 otherwise
+ * Return: Pointer to the modified string
  */
-int is_separator(char c)
+char *cap_string(char *str)
 {
 	char separators[] = " \t\n,;.!?\"(){}";
-	int i;
+	int capitalize_next = 1;
 
-	for (i = 0; separators[i] != '\0'; i++)
+	for (int i = 0; str[i] != '\0'; i++)
 	{
-		if (c == separators[i])
+		if (capitalize_next && str[i] >= 'a' && str[i] <= 'z')
 		{
-			return (1);
+			str[i] -= 32;
+			capitalize_next = 0;
+		}
+		else if (str[i] >= 'A' && str[i] <= 'Z')
+		{
+			str[i] += 32;
+			capitalize_next = 0;
+		}
+		else
+		{
+			capitalize_next = 0;
+		}
+
+		for (int j = 0; separators[j] != '\0'; j++)
+		{
+			if (str[i] == separators[j])
+			{
+				capitalize_next = 1;
+				break;
+			}
 		}
 	}
 
-	return 0;
+	return (str);
 }
 
