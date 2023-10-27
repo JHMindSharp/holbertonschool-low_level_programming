408c0776b1329de5843957cc55259b004cef7296
Miniknacky <Jonathanhuybrechts@gmail.com>
2023-10-27 06:56:27 -0700

    no

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -3,21 +3,22 @@
 /**
  * leet - Encodes a string into 1337.
  * @str: The input string to be encoded.
- * 
+ *
  * Return: A pointer to the encoded string.
  */
 char *leet(char *str)
 {
-       	char leet_map[10] = {'O', 'L', 0, 0, 'E', 0, 0, 'T', 'A', 0};
+	char letters[] = "aAeEoOtTlL";
+	char leet_code[] = "4433007711";
 	int i, j;
 
 	for (i = 0; str[i]; i++)
 	{
-		for (j = 0; j < 10; j++)
+		for (j = 0; letters[j]; j++)
 		{
-			if (str[i] == leet_map[j] || str[i] == leet_map[j] + 32)
+			if (str[i] == letters[j])
 			{
-				str[i] = '0' + j;
+				str[i] = leet_code[j];
 				break;
 			}
 		}
