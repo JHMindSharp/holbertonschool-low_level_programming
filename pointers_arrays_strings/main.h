b91c3aa872c9d784ffbac81c437ea24f4581e9c1
Miniknacky <Jonathanhuybrechts@gmail.com>
2023-10-27 08:39:50 -0700

    putchar

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -13,7 +13,6 @@ void puts2(char *str);
 void puts_half(char *str);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
-int _putchar(char *c);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
