794622e1471fb65598a9af5c93bf1818f5a97c95
Miniknacky <Jonathanhuybrechts@gmail.com>
2023-11-08 11:30:31 -0800

    task 2

---
+++ preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - this function will print the name of the source file.
+ *
+ * description: same than main.
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
