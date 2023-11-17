370a95611739024a5c750ae7a195495b75cd982e
Miniknacky <Jonathanhuybrechts@gmail.com>
2023-11-17 11:43:09 -0800

    -

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -3,38 +3,38 @@
 
 /**
  * print_char - Prints a character from va_list
- * @list: va_list to print from
+ * @args: va_list to print from
  */
-void print_char(va_list list)
+void print_char(va_list args)
 {
-	printf("%c", va_arg(list, int));
+	printf("%c", va_arg(args, int));
 }
 
 /**
  * print_int - Prints an integer from va_list
- * @list: va_list to print from
+ * @args: va_list to print from
  */
-void print_int(va_list list)
+void print_int(va_list args)
 {
-	printf("%d", va_arg(list, int));
+	printf("%d", va_arg(args, int));
 }
 
 /**
  * print_float - Prints a float from va_list
- * @list: va_list to print from
+ * @args: va_list to print from
  */
-void print_float(va_list list)
+void print_float(va_list args)
 {
-	printf("%f", (float)va_arg(list, double));
+	printf("%f", (float)va_arg(args, double));
 }
 
 /**
  * print_string - Prints a string from va_list
- * @list: va_list to print from
+ * @args: va_list to print from
  */
-void print_string(va_list list)
+void print_string(va_list args)
 {
-	char *str = va_arg(list, char *);
+	char *str = va_arg(args, char *);
 
 	if (str == NULL)
 		printf("(nil)");
@@ -50,29 +50,34 @@ void print_all(const char * const format, ...)
 {
 	va_list args;
 	int i = 0;
-	int j = 0;
-	char *separator = "";
+	int printed = 0;
+	char *sep = "";
 	char *types = "cifs";
-	void (*print_fn[4])(va_list) = {print_char, print_int, print_float,
-		print_string};
+	void (*print_fn[4])(va_list) = {print_char, print_int,
+		print_float, print_string};
 
 	va_start(args, format);
 	while (format && format[i])
 	{
-		j = 0;
+		int j = 0;
+
 		while (types[j])
 		{
-			if (types[j] == format[i])
+			if (format[i] == types[j])
 			{
-				printf("%s", separator);
+				printf("%s", sep);
 				print_fn[j](args);
-				separator = ", ";
+				sep = ", ";
+				printed = 1;
 			}
 			j++;
 		}
 		i++;
 	}
-	printf("\n");
 	va_end(args);
+	if (!printed)
+		printf("\n");
+	else
+		printf("\n");
 }
 
