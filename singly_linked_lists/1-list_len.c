c54f6cea4dc0a67b7c13beb21f366d0e0b12589b
Miniknacky <jonathanhuybrechts@gmail.com>
2023-11-27 18:44:45 +0000

    ajout commentaires et correction

--- singly_linked_lists/1-list_len.c
+++ singly_linked_lists/1-list_len.c
@@ -1,13 +1,19 @@
 #include "lists.h"
 
+/**
+ * list_len - fonction qui affiche le nombre d'elements dans la liste list_t.
+ * @h: pointeur qui pointe la fin du noeud.
+ *
+ * Return: la longueur de list_t.
+*/
 size_t list_len(const list_t *h)
 {
 	size_t count = 0;
 
-	while (h != NULL) {
-		count ++;
+	while (h != NULL)
+	{
+		count++;
 		h = h->next;
 	}
-
-	return count;
+	return (count);
 }
