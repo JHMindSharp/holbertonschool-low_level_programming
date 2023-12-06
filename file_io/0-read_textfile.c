c9fa2c079c087ca961ca7ca389ab232dbb4e75ae
Miniknacky <jonathanhuybrechts@gmail.com>
2023-12-06 10:48:44 +0000

    betty 0-read

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -12,27 +12,27 @@
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int file_descriptor, bytes_read, bytes_written;
-	char buffer[1024]; /* Adjust buffer size as needed */
+	char buffer[1024];
 
 	if (filename == NULL)
 		return (0);
 
 	file_descriptor = open(filename, O_RDONLY);
 	if (file_descriptor == -1)
-		return (0); /* Failed to open the file */
+		return (0);
 
 	bytes_read = read(file_descriptor, buffer, letters);
 	if (bytes_read == -1)
 	{
 		close(file_descriptor);
-		return (0); /* Failed to read the file */
+		return (0);
 	}
 
 	bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
 	close(file_descriptor);
 
 	if (bytes_written == -1 || bytes_written != bytes_read)
-		return (0); /* Write failed or didn't write expected bytes */
+		return (0);
 
 	return (bytes_written);
 }
